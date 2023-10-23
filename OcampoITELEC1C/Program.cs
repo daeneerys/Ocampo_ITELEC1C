8e2b753bfb16e302e132c81ca033b0fa7d7e0f3f
daeneerys <johndaniel.ocampo.cics@ust.edu.ph>
2023-10-23 11:38:39 +0800

    Oct 23, 2023

--- OcampoITELEC1C/Program.cs
+++ OcampoITELEC1C/Program.cs
@@ -1,10 +1,16 @@
+using Microsoft.AspNetCore.Connections;
+using Microsoft.EntityFrameworkCore;
+using OcampoITELEC1C.Data;
 using OcampoITELEC1C.Serivces;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddSingleton<IMyFakeDataService, MyFakeDataService>();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer
+(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllersWithViews();
+
 
 
 var app = builder.Build();
@@ -14,6 +20,9 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
+var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<AppDbContext>();
+context.Database.EnsureCreated();
 app.UseStaticFiles();
 
 app.UseRouting();
